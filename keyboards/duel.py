bc363de3c8ecc7a3785f6f1468cae99784242abc
cesardsn <139928465+cesardsn@users.noreply.github.com>
2025-12-23 20:30:03 -0300

    Delete keyboards directory

--- keyboards/duel.py
+++
@@ -1,9 +0,0 @@
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-
-
-def duel_menu():
-    return InlineKeyboardMarkup([
-        [InlineKeyboardButton("⚔️ Criar duelo", callback_data="duel_create")],
-        [InlineKeyboardButton("📥 Duelo disponível", callback_data="duel_list")],
-        [InlineKeyboardButton("⬅️ Voltar", callback_data="menu")]
-    ])
