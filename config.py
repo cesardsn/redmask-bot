8307c2962542886ae0c01b4836fb6f632065e7cf
cesardsn <139928465+cesardsn@users.noreply.github.com>
2025-12-23 21:25:00 -0300

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,16 @@ BOT_TOKEN = "8465541862:AAF01AdPI_CqTy5yBTMmYpffxr6AuI41yT4"
 TC_RECEIVER = "cesar eto"
 PREMIUM_PRICE = 25
 DAILY_LIMIT = 1
-DB_FILE = "storage/redmask.db"
+
+
+import os
+
+# Criar pasta de dados caso não exista
+DB_FOLDER = "./data"
+os.makedirs(DB_FOLDER, exist_ok=True)
+
+# Caminho do banco de dados
+DB_FILE = os.path.join(DB_FOLDER, "redmask_tibia.db")
 
 
 
